ece91832bbaf99ddecf590708fb5b5920d798ae2
John <john@john-XPS-M1330.(none)>
2011-08-24 14:49:48 -0500

    not version images, add styels

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,7 +2,7 @@ class Project < ActiveRecord::Base
   has_many :galleries, :dependent => :destroy
   accepts_nested_attributes_for :galleries, :reject_if => lambda { |a| a[:photo].blank? }, :allow_destroy => true
 
-  has_attached_file :photo, :styles => { :small => "137x110" }
+  has_attached_file :photo, :styles => { :small => "137x110", :large => "599X484" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 15.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
