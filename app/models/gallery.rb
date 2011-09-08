8c393bd39af21b44aa5ccab931a31d26aaea88f8
John <john@john-XPS-M1330.(none)>
2011-09-07 20:20:08 -0500

    success

--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,6 @@
 class Gallery < ActiveRecord::Base
   belongs_to :project
-  has_attached_file :photo, :styles => { :small => "137x110" }
+  has_attached_file :photo, :styles => { :medium => "411x330", :small => "137x110", :tiny => "10X10" }
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 5.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/png', 'image/gif']
