af0e434c958fc0a21b03a9f47235a6377a9ac1cc
John <john@john-XPS-M1330.(none)>
2011-09-07 21:51:55 -0500

    fix active link

--- app/assets/javascripts/alfa.js
+++ app/assets/javascripts/alfa.js
@@ -10,5 +10,14 @@ $(function() {
         $(this).parents("p:first").remove();
             return false;
     });
+
+    var url = window.location.pathname, 
+        urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+
+        $('a').each(function(){
+            if(urlRegExp.test(this.href.replace(/\/$/,''))){
+                $(this).addClass('active');
+            }
+        });
 });
 
