494fdf96accd712a3fab0a012d03855ff4e02591
john <john@john-Pangolin-Performance.(none)>
2011-09-29 17:54:34 -0400

    new

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,5 @@
 set :user, 'deploy'
-server "alfa.knockout.dontexist.org", :app, :web, :db, :primary => true
+server "alfa", :app, :web, :db, :primary => true
 
 set :rails_env, 'production'
 set :passenger_port, 3062
